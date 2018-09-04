3cf317b23ef1b60ab3d5551796ac82d774cf3071
BSMU\PishulenakNA <niki1995-11@mail.ru>
2018-09-04 15:19:43 +0300

    Задание 2. (Выполнено) Создать страницу с 3 кнопками При нажатии на первую - переход на другую(любую) страницу При нажатии на вторую – смена стиля страницы При нажатии на третью – удаление содержимого со страницы и отрисовка любого макета с flexbox

---
+++ 2/script.js
@@ -0,0 +1,31 @@
+//Ф-я изменения внешнего вида страницы
+function changeStyle(){
+	var btns=document.querySelectorAll('button');
+	for(var i=0; i<btns.length; i++){
+		btns[i].classList.add("btn");
+	}
+	document.querySelector('body').style.backgroundColor = '#c0c0c0';
+	var cont=document.getElementById('content');
+	cont.style.left = '50%';
+	cont.style.top = '50%';
+	cont.style.position = 'absolute';
+	cont.style.transform = 'translate(-50%, -50%)';
+}
+
+//Ф-я очистки экрана и рисования макета flexbox
+function writeFlex(){
+	document.querySelector('body').innerHTML="";
+	document.querySelector('body').style.backgroundColor = '';
+	var d=document.createElement('div');
+	d.className='container';
+	document.body.appendChild(d);
+	var rnd=Math.floor(Math.random() * 15) + 10;
+	for(var i=0; i<rnd; i++){
+		var div = document.createElement('div');
+		div.className='litleBlock';
+		div.innerHTML=i+1;
+		d.appendChild(div);
+	}
+	
+
+}
