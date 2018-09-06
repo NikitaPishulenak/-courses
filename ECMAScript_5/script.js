199f0553f6b30fe694fcb93da95942d275ed1b64
DESKTOP-9E3DMVG\niki1 <niki1995-11@mail.ru>
2018-09-06 21:01:07 +0300

    Написал функциональное наследование

--- ECMAScript_5/script.js
+++ ECMAScript_5/script.js
@@ -1,31 +1,27 @@
 //Ф-я изменения внешнего вида страницы
-function changeStyle(){
-	var btns=document.querySelectorAll('button');
-	for(var i=0; i<btns.length; i++){
-		btns[i].classList.add("btn");
-	}
-	document.querySelector('body').style.backgroundColor = '#c0c0c0';
-	var cont=document.getElementById('content');
-	cont.style.left = '50%';
-	cont.style.top = '50%';
-	cont.style.position = 'absolute';
-	cont.style.transform = 'translate(-50%, -50%)';
-}
+function Book(title, author, pubOffice, pubYear, faculty, price){
+	this.title=title;
+	this.author=author;
+	this.pubOffice=pubOffice;
+	this.pubYear=pubYear;
+	this.faculty=faculty;
+	this.price=price;
 
-//Ф-я очистки экрана и рисования макета flexbox
-function writeFlex(){
-	document.body.innerHTML="";
-	document.body.style.backgroundColor = '';
-	var d=document.createElement('div');
-	d.className='container';
-	document.body.appendChild(d);
-	var rnd=Math.floor(Math.random() * 15) + 10;
-	for(var i=0; i<rnd; i++){
-		var div = document.createElement('div');
-		div.className='litleBlock';
-		div.innerHTML=i+1;
-		d.appendChild(div);
+	this.getFullInfoAboutBook=function(){
+		return "Автор книги: "+ this.author + "\nНазвание книги: "+ this.title +"\nИдательство: "+this.pubOffice;
 	}
 	
-
 }
+
+function Audio(title, author, pubOffice, pubYear, faculty, price, duration, format){
+	Book.apply(this, arguments);
+	this.duration=duration;
+	this.format=format;
+}
+
+function Manual(title, author, pubOffice, pubYear, faculty, price, bindingType, complementCD){
+	Book.apply(this, arguments);
+	this.bindingType=bindingType;
+	this.complementCD=complementCD;
+}
+
